8b7eca3eb0fd3280fe590e77264ac50e58aa95bc
sjagadee <sjagadee@hawk.iit.edu>
2017-02-07 23:48:31 -0600

    Changes in code

--- app/src/main/java/com/hello/android/srinivas/personalorganizer/AddNewItem.java
+++ app/src/main/java/com/hello/android/srinivas/personalorganizer/AddNewItem.java
@@ -2,6 +2,7 @@ package com.hello.android.srinivas.personalorganizer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by srinivas on 2/5/17.
@@ -9,11 +10,14 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AddNewItem extends AppCompatActivity {
 
+    private Toolbar toolbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_item);
 
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 }
